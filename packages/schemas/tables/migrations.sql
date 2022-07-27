150550c91e1fe5ab32015d04b365739726823982
wangsijie <wangsijie@silverhand.io>
2022-07-27 14:14:28 +0800

    feat(schemas): add migrations table

---
+++ packages/schemas/tables/migrations.sql
@@ -0,0 +1,9 @@
+create table _migrations (
+  id varchar(128) not null,
+  name varchar(256) not null unique,
+  checksum varchar(64) not null,
+  started_at timestamptz not null default(now()),
+  finished_at timestamptz,
+  rolled_back_at timestamptz,
+  primary key (id)
+);
